d7a83cdfe34790ef5c469d102166199e075e0ba0
Harley Rayner <harleymteden@gmail.com>
2017-12-04 18:49:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if (number % 3 == 0) && (number % 5 == 0)
+  "FizzBuzz"
+  elsif number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  else (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
  end
 end
